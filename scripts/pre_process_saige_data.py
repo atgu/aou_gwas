b29262abb43ff95e0980fdeef2da5ce140ae5f33
wlu04 <wlu@broadinstitute.org>
2023-07-13 18:24:11 -0400

    minor fix

--- scripts/pre_process_saige_data.py
+++ scripts/pre_process_saige_data.py
@@ -18,6 +18,7 @@ RVAS_AF_CUTOFF = 0.01
 CALLRATE_CUTOFF = 0.8
 N_GENE_PER_GROUP = 10
 CURRENT_BUCKET = f'{BUCKET}/{TRANCHE}'
+POPS = ('afr', 'amr', 'eas', 'eur', 'mid', 'sas')
 
 # ALL PATHs: https://support.researchallofus.org/hc/en-us/articles/4616869437204-Controlled-CDR-Directory
 # Descriptions: https://support.researchallofus.org/hc/en-us/articles/4614687617556-How-the-All-of-Us-Genomic-data-are-organized
@@ -37,7 +38,10 @@ def do_annotate(mt, meta_ht, by_col=True):
     return mt
 
 def get_aou_vep_path(tranche: str = TRANCHE):
-    return f'{CURRENT_BUCKET}/vep/aou_vep_{tranche}.ht'
+    return f'{CURRENT_BUCKET}/util/aou_vep_{tranche}.ht'
+
+def get_aou_vat_path(tranche: str = TRANCHE):
+    return f'{CURRENT_BUCKET}/util/aou_vat_{tranche}.ht'
 
 def get_aou_grm_mt_path(pop: str, data_iteration:int, tranche: str = TRANCHE):
     suffix = f'_{data_iteration}' if data_iteration else ""
@@ -52,13 +56,44 @@ def get_aou_grm_plink_path(pop: str, data_iteration: int = 0, window_size: str =
     cut = '' if window_size == '1e6' else f'.{window_size}'
     return f'{CURRENT_BUCKET}/grm/aou_{pop}_for_grm{suffix}_pruned{cut}_{tranche}.plink'
 
+def get_vat_field_types():
+    tags = ('afr', 'amr', 'asj', 'eas', 'eur', 'fin', 'mid', 'nfr', 'sas', 'oth', 'max', 'all')
+    fields = ('ac', 'an', 'sc')
+    types = {}
+    for tag in tags:
+        types[f'gvs_{tag}_af'] = hl.tfloat64
+        types[f'gnomad_{tag}_af'] = hl.tfloat64
+        for field in fields:
+            types[f'gvs_{tag}_{field}'] = hl.tint32
+            types[f'gnomad_{tag}_{field}'] = hl.tint32
+    types['is_canonical_transcript'] = hl.tbool
+    types['omim_phenotypes_id'] = hl.tarray(hl.tint32)
+    for x in ['position', 'gene_omim_id', 'splice_ai_acceptor_gain_distance', 'splice_ai_acceptor_loss_distance',
+              'splice_ai_donor_gain_distance', 'splice_ai_donor_loss_distance']:
+        types[x] = hl.tint32
+    for x in ['revel', 'splice_ai_acceptor_gain_score', 'splice_ai_acceptor_loss_score', 'splice_ai_donor_gain_score',
+              'splice_ai_donor_loss_score']:
+        types[x] = hl.tfloat64
+    for x in ['omim_phenotypes_name', 'clinvar_classification', 'clinvar_phenotype', 'consequence', 'dbsnp_rsid']:
+        types[x] = hl.tarray(hl.tstr)
+    return types
+
 
 def main(args):
     hl.init(default_reference='GRCh38')
+
+    # Load and VDS and split-multi
     vds = hl.vds.read_vds(WGS_VDS_PATH)
-    # vmt: (1031611675, 245394), rmt: (2853640141, 245394)
-    vat_ht = hl.import_table(VAT_TSV_PATH, force=True, quote='"', delimiter="\t", force_bgz=True, impute=True)
-    vat_ht = vat_ht.filter(vat_ht.gvs_max_ac > 0)
+    # Pre splitting multi: vmt: (1031611675, 245394), rmt: (2853640141, 245394)
+    vds = hl.vds.split_multi(vds, filter_changed_loci=False)
+
+    # Load variant info table and re-format
+    vat_ht = hl.import_table(VAT_TSV_PATH, force=True, quote='"', delimiter="\t", force_bgz=True, types = get_vat_field_types())
+    vat_ht = vat_ht.annotate(locus=hl.locus(vat_ht.contig, vat_ht.position, reference_genome='GRCh38'),
+                             alleles=[vat_ht.ref_allele, vat_ht.alt_allele])
+    vat_ht = vat_ht.key_by('locus', 'alleles')
+    vat_ht = vat_ht.checkpoint(get_aou_vat_path(), _read_if_exists=not args.overwrite, overwrite=args.overwrite)
+    vat_ht = vat_ht.filter(vat_ht.gvs_all_ac > 0)
     pop_ht = hl.import_table(ANCESTRY_PATH, key="research_id", impute=True,
                              types={"research_id": "tstr", "pca_features": hl.tarray(hl.tfloat)})
     # {'afr': 56913, 'amr': 45035, 'eas': 5706, 'eur': 133581, 'mid': 942, 'sas': 3217} - Total: 245394
@@ -92,7 +127,7 @@ def main(args):
 
             # Filter variants
             call_stats_ht = vat_ht.select(
-                AF=vat_ht[f'gvs_{pop}_af'],
+                AF=hl.float64(vat_ht[f'gvs_{pop}_af']),
                 AC=vat_ht[f'gvs_{pop}_ac'],
                 AN=vat_ht[f'gvs_{pop}_an'],
                 N=vat_ht[f'gvs_{pop}_sc'],
@@ -136,6 +171,38 @@ def main(args):
                 print(f'Exporting plink to {get_aou_grm_plink_path(pop, iteration, window)}...')
                 hl.export_plink(mt, get_aou_grm_plink_path(pop, iteration, window))
 
+    if args.genotype_summary:
+        variants = hl.read_table(ukb_imputed_info_ht_path)
+        print(variants.count())
+        variants = variants.filter(variants.info > 0.8)
+        print(variants.count())
+        meta_ht = hl.import_table(get_ukb_meta_pop_tsv_path(), impute=True, types={'s': hl.tstr}, key='s')
+
+        mt = get_ukb_imputed_data('all', variant_list=variants, entry_fields=('dosage', ))
+        mt = mt.annotate_cols(**meta_ht[mt.col_key])
+        ht = mt.annotate_rows(af=hl.agg.group_by(mt.pop, hl.agg.mean(mt.dosage)),
+                              an=hl.agg.group_by(mt.pop, hl.agg.count_where(hl.is_defined(mt.dosage)))).rows()
+        ht = ht.checkpoint(get_ukb_af_ht_path(False), args.overwrite, _read_if_exists=not args.overwrite)
+
+        mt = get_ukb_imputed_data('X', variant_list=variants, entry_fields=('dosage', ))
+        mt = mt.annotate_cols(**meta_ht[mt.col_key])
+        ht_x = mt.annotate_rows(af=hl.agg.group_by(mt.pop, hl.agg.mean(mt.dosage)),
+                              an=hl.agg.group_by(mt.pop, hl.agg.count_where(hl.is_defined(mt.dosage)))).rows()
+        ht = ht.union(ht_x)
+        ht = ht.checkpoint(get_ukb_af_ht_path(), args.overwrite, _read_if_exists=not args.overwrite)
+        ht = ht.naive_coalesce(1000).checkpoint(get_ukb_af_ht_path(repart=True), args.overwrite, _read_if_exists=not args.overwrite)
+
+        print(ht.aggregate(hl.struct(
+            # hist=hl.agg.hist(hl.sum(ht.an.values()), 0, total_samples, 10),  # No missing data
+            # fraction_missingness=hl.agg.fraction(hl.sum(ht.an.values()) < total_samples),
+            # number_sites_above_001=hl.agg.count_where(hl.any(lambda x: x / 2 > 0.001, ht.af.values())),
+            # number_sites_above_005=hl.agg.count_where(hl.any(lambda x: x / 2 > 0.005, ht.af.values())),
+            # number_sites_above_01=hl.agg.count_where(hl.any(lambda x: x / 2 > 0.01, ht.af.values())),
+            # number_sites_above_05=hl.agg.count_where(hl.any(lambda x: x / 2 > 0.05, ht.af.values())),
+            # number_sites_above_10=hl.agg.count_where(hl.any(lambda x: x / 2 > 0.1, ht.af.values()))
+            number_sites_above_mac_20=hl.agg.count_where(hl.any(lambda x: ht.af[x] * ht.an[x] >= 20, hl.literal(POPS))),
+            number_run_sites_above_mac_20=hl.agg.sum(hl.sum(hl.map(lambda x: hl.int(ht.af[x] * ht.an[x] >= 20), hl.literal(POPS))))
+        )))
 
 
 if __name__ == '__main__':
